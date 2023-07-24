aa68eb6824953bdea2573fe9b7a11676871f064b
Shantel Nubian <nubian.evolutionary@gmail.com>
2023-07-24 11:07:36 -0400

    modified routes, imported bodyParser, changed video image url

--- index.js
+++ index.js
@@ -1,18 +1,23 @@
-//server
-//final step
+const bodyParser = require('body-parser')
 
 const express = require('express') // gets access to http via express library
-const videoRouter = require('./routes/videos')
+const cors = require('cors') 
 const port = 5000
 const server = express() // server is running on express js library
+//server.use(express.json());// command enables server to read json
+server.use(bodyParser.urlencoded({extended: true})); // enable raw data to be parsed since consistent errors
+server.use(cors())
+const videoRouter = require('./routes/videos.js')
+
+
 server.get('/', (req, res) => {
     res.json({ "Welcome": "Hello Developers!"});
 }
 
+
 );
 
 server.use('/', videoRouter);
-
 server.listen(port, function () {
         console.log("Your Server is Running");
     }) // server listen to port 5000
--- routes/videos.js
+++ routes/videos.js
@@ -1,25 +1,29 @@
 // express is the object that reates the logic for the routes of data
-
 const express = require('express');
 const router = express.Router(); //router = mini app
 const fs = require('fs');
 const {v4: uuidv4} = require('uuid'); // generates a unique ID
+const bodyParser = require('body-parser'); //import body parser
 
-
-
+router.use(bodyParser.json());// commands executes body parser for json files to the code
+const getVideos = () => {
+    let data = fs.readFileSync('./data/videos.json')
+    return JSON.parse(data)
+}
 //Req 1: Get an array of Videos
 
 router.get('/videos', function(request, response) {
-    const videosArray = require('../data/videos.json'); // get data from video file and put it into json format
+    const videosArray = getVideos(); // get data from video file and put it into json format
     response.header('Content-Type', 'application/json'); // adds a header to response
     response.json(videosArray); // gets json response to the client
+    
 });
 
 //Req 2: GET /videos/:id that responds with an object containing the details of the video with an id of :id
 //https://sn.com/videos/67
 router.get('videos/:id', function(req, res)
 {
-    const videosArray = require('../data/videos.json');
+    const videosArray = getVideos();
     res.header('Content-Type', 'application/json') ;
     
     for (let i = 0; i < videosArray.length; i++) {
@@ -36,20 +40,17 @@ router.get('videos/:id', function(req, res)
 //Req 3: POST /videos that will add a new video to the video list. A unique id must be generated for each video added.
 router.post('/videos', (req, res)  => {
 
-    // data should have an attribute called title
+  // data should have an attribute called title
     // and channel
-
-    let video = req.body();
+    let video = req.body;
     
     // generate a unique id for the video
     const uuid = new uuidv4();
 
     // put the id on the video
     video.id = uuid;
-
-
     // set the image of the video
-    video.image = 'default.png';
+    video.image = 'https://127.0.0.1:5000/default.png';
 
     // get the current videos in our videos.json
     // then we add this new one and save all of
@@ -57,7 +58,7 @@ router.post('/videos', (req, res)  => {
     // always have an updated videos array in
     // the file.
 
-    const videosArray = require('../data/videos.json');
+    const videosArray = getVideos();
     videosArray.push(video);
     
     // convert the videos array to a string 
@@ -69,7 +70,7 @@ router.post('/videos', (req, res)  => {
 
     // fs writes newly stringified data (updated videosArray) to the 
     // videos.json file
-    fs.writeFileSync('../data/videos.json', videosArrayString, 'utf8', (error) => {
+    fs.writeFileSync('./data/videos.json', videosArrayString, 'utf8', (error) => {
         
         if(error) {
             // if there is an error
@@ -86,6 +87,9 @@ router.post('/videos', (req, res)  => {
             return res.json({message: 'successfully saved the video', video});
         }
     });
+
 });
 
 module.exports = router; //exporting our mini app to the server
+
+
